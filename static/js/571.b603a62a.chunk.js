79e845ff6a73092a5d9512ab2b77d214cf2b8fd7
KolyaKhmarjuk <106625975+KolyaKhmarjuk@users.noreply.github.com>
2023-06-06 15:38:43 +0000

    Deploying to gh-pages from @ KolyaKhmarjuk/react-phonebook-8@363e22ac5af2299b61460de35bf9248318d06207 🚀

---
+++ static/js/571.b603a62a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_phonebook_8=self.webpackChunkreact_phonebook_8||[]).push([[571],{5571:function(e,n,c){c.r(n);c(2791);var o=c(3329);n.default=function(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"Home"})})}}}]);
+//# sourceMappingURL=571.b603a62a.chunk.js.map
